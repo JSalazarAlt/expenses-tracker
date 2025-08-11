9eadd56a3dbe30cddfddcbbe7a45186e64f43567
JSalazarAlt <ed.joel.salazar@gmail.com>
2025-08-10 23:24:01 -0500

    Updating interactive selection of expense chart

--- src/main/resources/static/js/expense-chart.js
+++ src/main/resources/static/js/expense-chart.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
                          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    // Create chart instance with empty data to start
+    // Create chart instance with empty data
     const expensesChart = new Chart(ctx, {
         type: 'bar',
         data: {
